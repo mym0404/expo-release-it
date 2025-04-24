8455e84fcfcdefa09d3e00f1a835a50f122570ce
이병문명주 <mym0404@gmail.com>
2025-04-24 13:50:39 +0900

    chore: unconscious commit

--- src/util/parseBinaryVersion.ts
+++ src/util/parseBinaryVersion.ts
@@ -2,6 +2,7 @@ import fs from 'fs-extra';
 import { OptionHolder } from './OptionHolder';
 import * as path from 'path';
 import { throwError } from './throwError';
+import semver from 'semver';
 
 export async function parseBinaryVersion() {
   const files = await fs.readdir(OptionHolder.rootDir);
@@ -37,6 +38,10 @@ export async function parseBinaryVersion() {
       throwError('cannot parse version code');
     }
 
+    if (semver.valid(VERSION_NAME)) {
+      throwError('VERSION_NAME is not a valid semver format');
+    }
+
     OptionHolder.versionName = VERSION_NAME;
     OptionHolder.versionCode = VERSION_CODE;
   }
@@ -45,19 +50,27 @@ export async function parseBinaryVersion() {
     const filePath = path.resolve(OptionHolder.rootDir, filename);
     const json = JSON.parse(await fs.readFile(filePath, { encoding: 'utf-8' }));
 
-    const versionName = json.expo.version;
+    const versionName = json.expo?.version + '';
+    const iosVersionCode = json.expo?.ios?.buildNumber + '';
+    const androidVersionCode = json.expo?.android?.versionCode + '';
 
-    const VERSION_NAME = /const VERSION_NAME = '(.*?)';?/.exec(content)?.[1];
-    const VERSION_CODE = /const VERSION_CODE = (.*?);?/.exec(content)?.[1];
-
-    if (!VERSION_NAME) {
-      throwError('cannot parse version name');
-    }
-    if (!VERSION_CODE) {
-      throwError('cannot parse version code');
+    if (!/\d+/.test(iosVersionCode)) {
+      throwError('app.json expo.ios.buildNumber is not an integer');
     }
 
-    OptionHolder.versionName = VERSION_NAME;
-    OptionHolder.versionCode = VERSION_CODE;
+    if (!/\d+/.test(androidVersionCode)) {
+      throwError('app.json expo.android.versionCode is not an integer');
+    }
+
+    if (iosVersionCode !== androidVersionCode) {
+      throwError('app.json expo.ios.buildNumber is not equal with expo.android.versionCode');
+    }
+
+    if (semver.valid(versionName)) {
+      throwError('VERSION_NAME is not a valid semver format');
+    }
+
+    OptionHolder.versionName = versionName;
+    OptionHolder.versionCode = iosVersionCode;
   }
 }

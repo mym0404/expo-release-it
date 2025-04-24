28cccb41386a4bfd18ecae0f9842b72437f78f87
이병문명주 <mym0404@gmail.com>
2025-04-24 13:51:57 +0900

    chore: unconscious commit

--- src/command/bump.ts
+++ src/command/bump.ts
@@ -1,7 +1,30 @@
 import { parseBinaryVersion } from '../util/parseBinaryVersion';
 import { promptCommonInputs } from '../util/promptCommonInputs';
+import { execa } from 'execa';
+import { throwError } from '../util/throwError';
 
 export async function bump() {
+  await preCheck();
   await promptCommonInputs();
   await parseBinaryVersion();
+}
+
+async function preCheck() {
+  try {
+    // await spinner('gen strings', () => $`yarn l && yarn c`);
+
+    let hasChange = false;
+    try {
+      await execa`git diff --quiet HEAD`;
+    } catch (_) {
+      hasChange = true;
+    }
+    if (hasChange) {
+      throwError('Workspace is not clean. Before bump version, you should commit all changes.');
+    }
+
+    // await spinner('static checking', () => $`yarn t`);
+  } catch (e) {
+    throwError('Precheck Failed', e);
+  }
 }

954fe864b75c15f0f32c22312e04b3536fa64e7c
MJ <mym0404@gmail.com>
2025-04-29 18:25:00 +0900

    fix: pass valid entry files in windows OS

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'tsup';
 import glob from 'tiny-glob';
+import * as path from 'node:path';
 
 export default defineConfig(async () => {
+  const entries = await glob('./src/**/!(*.d|*.spec).ts');
+  const normalizedEntries = entries.map((p) => p.split(path.sep).join('/'));
   return [
     {
-      entry: await glob('./src/**/!(*.d|*.spec).ts'),
+      entry: normalizedEntries,
       splitting: false,
       sourcemap: true,
       clean: true,

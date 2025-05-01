84816f8266e5c63b16d165c827b3e88fc305c25a
Mystic <mym0404@gmail.com>
2025-05-01 22:31:17 +0900

    docs: setup nextra

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -2,7 +2,7 @@ import nextra from 'nextra';
 
 // Set up Nextra with its configuration
 const withNextra = nextra({
-  // ... Add Nextra-specific options here
+  // contentDirBasePath: '/docs',
 });
 
 // Export the final Next.js config with Nextra included
@@ -10,7 +10,7 @@ export default withNextra({
   turbopack: {
     resolveAlias: {
       // Path to your `mdx-components` file with extension
-      'next-mdx-import-source-file': './src/mdx-components.tsx',
+      // 'next-mdx-import-source-file': './src/mdx-components.tsx',
     },
   },
 });

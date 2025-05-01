2bd09a3b8bd944c2958d328b4fd7a0b387916d0d
이병문명주 <mym0404@gmail.com>
2025-05-01 16:02:25 +0900

    test: setup vitest and add sample tests for VersionUtils

--- src/util/throwError.ts
+++ src/util/throwError.ts
@@ -1,10 +1,14 @@
 import { logger } from './logger';
+import { isTest } from './EnvUtil';
 
 export function throwError(message: string, e?: any): never {
   if (e) {
     logger.error(message, e);
   } else {
     logger.error(message);
+  }
+  if (isTest) {
+    throw new Error('throwError');
   }
   process.exit(1);
 }

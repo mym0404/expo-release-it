5f79ab45f1e82fb60d159e969de0adde0d1c194b
MJ <mym0404@gmail.com>
2025-09-01 22:54:56 +0900

    chore: unconscious commit 🎲

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -10,6 +10,7 @@ const withNextra = nextra({
 
 // Export the final Next.js config with Nextra included
 export default withNextra({
+  output: 'standalone',
   turbopack: {
     resolveAlias: {
       // Path to your `mdx-components` file with extension
